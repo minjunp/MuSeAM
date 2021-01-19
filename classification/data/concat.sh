d1f050fb8a2640f1f7a2326c1839f061239a8b38
minjunp <mp44@rice.edu>
2021-01-19 12:22:39 -0600

    Updated Jan 19 2021

--- classification/data/concat.sh
+++ classification/data/concat.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-pos_fa="E13RACtrlF1_E13RAMutF1_DMR_toppos2000_pos_v2.fa"
-neg_fa="E13RACtrlF1_E13RAMutF1_DMR_toppos2000_neg_v2.fa"
-combined_fa="E13RACtrlF1_E13RAMutF1_DMR_toppos2000_combined.fa"
-readout_name="E13RACtrlF1_E13RAMutF1_DMR_toppos2000.dat"
+pos_fa=$(cat config.csv | awk 'NR==2' | cut -f6 -d',')
+neg_fa=$(cat config.csv | awk 'NR==2' | cut -f7 -d',')
+combined_fa=$(cat config.csv | awk 'NR==2' | cut -f8 -d',')
+readout_name=$(cat config.csv | awk 'NR==2' | cut -f9 -d',')
+#pos_fa="E13RACtrlF1_E13RAMutF1_DMR_toppos2000_pos_v2.fa"
+#neg_fa="E13RACtrlF1_E13RAMutF1_DMR_toppos2000_neg_v2.fa"
+#combined_fa="E13RACtrlF1_E13RAMutF1_DMR_toppos2000_combined.fa"
+#readout_name="E13RACtrlF1_E13RAMutF1_DMR_toppos2000.dat"
 
 cat ${pos_fa} ${neg_fa} > ${combined_fa}
 
