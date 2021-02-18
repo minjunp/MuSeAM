858bfa3de9e10039fa079fa61353165bbcb3aa81
minjunp <minjunp3@gmail.com>
2021-02-18 15:47:49 -0600

    updated feb 18 2021

--- regression/search_param.sh
+++ regression/search_param.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 work_dir="/project/samee/minjun/MuSeAM/regression"
-output_base_dir="${work_dir}/outs/performance"
-par_output_dir="${work_dir}/outs/pars"
+output_base_dir="${work_dir}/outs_optimizer/performance"
+par_output_dir="${work_dir}/outs_optimizer/pars"
 fasta_file="${work_dir}/sequences.fa"
 readout_file="${work_dir}/wt_readout.dat"
 train_file="${work_dir}/train.py"
@@ -20,9 +20,9 @@ mkdir -p ${par_output_dir}
 #code="/project/samee/minjun/mpra/code/dummy.py"
 
 index=0
-for filters in 300 512
+for filters in 512
 do
-  for kernel_size in 16
+  for kernel_size in 12
   do
     for pool_type in Max
     do
@@ -30,33 +30,37 @@ do
       do
         for activation_type in linear
         do
-          for epochs in 30 40 50
+          for epochs in 100
           do
-            for batch_size in 16
+            for batch_size in 61
             do
-              for loss_func in mse poisson huber log_cosh
+              for loss_func in mse
               do
-                let index=index+1
-                parameter_file_name="${par_output_dir}/parameters_"${index}".txt"
-                echo "filters "${filters} > ${parameter_file_name}
-                echo "kernel_size "${kernel_size} >> ${parameter_file_name}
-                echo "pool_type "${pool_type} >> ${parameter_file_name}
-                echo "regularizer "${regularizer} >> ${parameter_file_name}
-                echo "activation_type "${activation_type} >> ${parameter_file_name}
-                echo "epochs "${epochs} >> ${parameter_file_name}
-                echo "batch_size "${batch_size} >> ${parameter_file_name}
-                echo "loss_func "${loss_func} >> ${parameter_file_name}
-                output_dir="${output_base_dir}/out_${index}"
-                #if [ -d ${output_dir} ]
-                #then
-                #  rm -rf ${output_dir}
-                #fi
-                #mkdir -p ${output_dir}
+                for optimizer in adam RMSprop SGD Adadelta Adagrad Adamax Nadam Ftrl
+                do
+                  let index=index+1
+                  parameter_file_name="${par_output_dir}/parameters_"${index}".txt"
+                  echo "filters "${filters} > ${parameter_file_name}
+                  echo "kernel_size "${kernel_size} >> ${parameter_file_name}
+                  echo "pool_type "${pool_type} >> ${parameter_file_name}
+                  echo "regularizer "${regularizer} >> ${parameter_file_name}
+                  echo "activation_type "${activation_type} >> ${parameter_file_name}
+                  echo "epochs "${epochs} >> ${parameter_file_name}
+                  echo "batch_size "${batch_size} >> ${parameter_file_name}
+                  echo "loss_func "${loss_func} >> ${parameter_file_name}
+                  echo "optimizer "${optimizer} >> ${parameter_file_name}
+                  output_dir="${output_base_dir}/out_${index}"
+                  #if [ -d ${output_dir} ]
+                  #then
+                  #  rm -rf ${output_dir}
+                  #fi
+                  #mkdir -p ${output_dir}
 
-                python ${train_file} ${fasta_file} ${readout_file} ${parameter_file_name} > ${output_base_dir}/${index}.txt
+                  python ${train_file} ${fasta_file} ${readout_file} ${parameter_file_name} > ${output_base_dir}/${index}.txt
 
-                #echo "output_dir "${output_dir} >> ${parameter_file_name}
-            		#echo "sleep 5m" >> ${exec_file}
+                  #echo "output_dir "${output_dir} >> ${parameter_file_name}
+              		#echo "sleep 5m" >> ${exec_file}
+                done
               done
             done
           done
