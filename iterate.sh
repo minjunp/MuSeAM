7f498150ef879030ad4c7096675ddd4b71d1a303
minjun <minjunp3@gmail.com>
2021-06-10 14:52:07 +0900

    updated silencer

--- iterate.sh
+++ iterate.sh
@@ -1,7 +1,7 @@
-python train.py ./data/simulated_data/set_1/combined.fa ./data/simulated_data/set_1/readout.dat parameter.txt > ./logs/data1_out
+#!/bin/bash
+mkdir outs
 
-python train.py ./data/simulated_data/set_2/combined.fa ./data/simulated_data/set_2/readout.dat parameter.txt > ./logs/data2_out
-
-python train.py ./data/simulated_data/set_3/combined.fa ./data/simulated_data/set_3/readout.dat parameter.txt > ./logs/data3_out
-
-python train.py ./data/simulated_data/set_4/combined.fa ./data/simulated_data/set_4/readout.dat parameter.txt > ./logs/data4_out
+for i in $(seq 1 30)
+do
+python train.py ./data/silencer/sequences.fa ./data/silencer/wt_readout.dat parameter.txt > ./outs/${i}.txt
+done
