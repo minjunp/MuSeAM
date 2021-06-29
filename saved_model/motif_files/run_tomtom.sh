c560bba56e37328a2e19a55b020171c236222609
Minjun Park <minjunpark@imac.ad.bcm.edu>
2021-06-29 02:31:51 -0500

    hocomoco motif code

---
+++ saved_model/motif_files/run_tomtom.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+database_file="$(pwd)/HOCOMOCOv11_full_HUMAN_mono_meme_format.meme"
+pwms="$(pwd)/pwms"
+
+tomtom_output="$(pwd)/tomtom_outputs"
+if [ -d ${tomtom_output} ]
+then
+	rm -rf ${tomtom_output}
+fi
+mkdir -p ${tomtom_output}
+
+for f in `ls ${pwms}`
+do
+	n=`echo $f | cut -d"_" -f2 | cut -d"." -f1`
+	#echo $n
+	pwm_file="${pwms}/pwm_${n}.mat"
+	output_dir="${tomtom_output}/result_${n}"
+	#mkdir -p ${output_dir}
+	#tomtom [options] <query file> <target file>+
+	tomtom -oc ${output_dir} ${pwm_file} ${database_file}
+done
---
+++ saved_model/save_model.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import numpy as np
+
+def save_model(self, model, alpha, path):
+    os.makedirs(f'{path}/motif_original')
+    os.makedirs(f'{path}/motif_files')
+    model.save(f'{path}/regression_model')
+    #model.save_weights(f'{path}/checkpoint')
+
+    motif_weight = model.get_weights()
+    motif_weight = np.asarray(motif_weight[0])
+
+    for i in range(self.filters):
+        np.savetxt(f'{path}/motif_original/filter_num_{"{0:0=3d}".format(i)}.txt', motif_weight[:,:,i])
+
+    for i in range(self.filters):
+        x = motif_weight[:,:,i]
+        berd = np.divide(np.exp(alpha*x), np.transpose(np.expand_dims(np.sum(np.exp(alpha*x), axis = 1), axis = 0), [1,0]))
+        np.savetxt(f'{path}/motif_files/filter_num_{"{0:0=3d}".format(i)}.txt', berd)
